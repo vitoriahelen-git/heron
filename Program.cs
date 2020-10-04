35036b2c2566e7f7f99551a8a1e8e14e75129006
Vitoria <vitoriahellen3004@gmail.com>
2020-10-03 21:37:47 -0300

    Calcule a área de um triângulo qualquer, dadas as medidas dos 3 lados. Exiba o semiperímetro e a área.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace heron
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double lado1;
+            double lado2;
+            double lado3;
+            double semiperimetro;
+            double area; 
+
+            Console.WriteLine("Digite os lados do triângulo desejado: \n");
+            Console.Write("Lado 1: ");
+            lado1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Lado 2: ");
+            lado2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Lado 3: ");
+            lado3 = Convert.ToDouble(Console.ReadLine());
+            semiperimetro = (lado1 + lado2 + lado3) /2; 
+            Console.WriteLine($"\nSemiperímetro {semiperimetro}");
+            area = semiperimetro * (semiperimetro - lado1) * (semiperimetro - lado2) * (semiperimetro - lado3);
+            Console.WriteLine($"Area {Math.Sqrt(area)}");
+            
+    
+
+
+
+        }
+    }
+}
